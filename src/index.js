29611927bd084fb06b62d8305c5948bab5cd6427
asdacosta <asdacosta18@gmail.com>
2023-12-05 21:42:32 +0000

    Comment out unused import

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './reset.css';
 import './style.css';
 // import './assets/###.mp4';
 
-const importAllAssets = (function () {
-  function importAll(r) {
-    return r.keys().map(r);
-  }
+// const importAllAssets = (function () {
+//   function importAll(r) {
+//     return r.keys().map(r);
+//   }
 
-  const assets = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
-})();
+//   const assets = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
+// })();
