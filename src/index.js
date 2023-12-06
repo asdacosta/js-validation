3ab5a0f51d088f9e74521efea757deb7f83a9565
asdacosta <asdacosta18@gmail.com>
2023-12-06 00:36:08 +0000

    Add displaying and closing of form functions

--- src/index.js
+++ src/index.js
@@ -9,3 +9,25 @@ import './style.css';
 
 //   const assets = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
 // })();
+
+const displayForm = (function () {
+  const displayFormButton = document.querySelector('body > button');
+  const mainSection = document.querySelector('.main-sec');
+  const overlay = document.querySelector('body > div');
+
+  displayFormButton.addEventListener('click', () => {
+    mainSection.classList.add('active');
+    overlay.classList.add('overlay');
+  });
+
+  return { mainSection, overlay };
+})();
+
+const exitForm = (function () {
+  const exit = document.querySelector('.form-sec > div:first-child');
+
+  exit.addEventListener('click', () => {
+    displayForm.mainSection.classList.remove('active');
+    displayForm.overlay.classList.remove('overlay');
+  });
+})();
