35d5f7c1644fcbf46fd0c181617ae076106592fa
asdacosta <asdacosta18@gmail.com>
2023-12-06 00:43:37 +0000

    Remove trivial comment

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
 import './reset.css';
 import './style.css';
-// import './assets/###.mp4';
-
-// const importAllAssets = (function () {
-//   function importAll(r) {
-//     return r.keys().map(r);
-//   }
-
-//   const assets = importAll(require.context('./assets', false, /\.(png|jpe?g|svg)$/));
-// })();
 
 const displayForm = (function () {
   const displayFormButton = document.querySelector('body > button');
